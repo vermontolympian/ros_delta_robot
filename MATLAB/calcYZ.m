e66ca93e0eab2e9fb289d1ffa88ef31eb93bcf75
Brandon <2boog25@gmail.com>
2023-07-26 20:24:03 -0400

    Moved files

--- MATLAB/calcYZ.m
+++ MATLAB/calcYZ.m
@@ -1,13 +1,16 @@
-function theta = calcYZ(p, upper, lower, Rf, Rm)
-    y1 = -Rf;
-    y = -Rm;
-    a = (x*x + y*y + z*z + upper*upper - lower*lower - y1*y1)/(2*z);
+function theta = calcYZ(p, end_effector_rod, servo_arm, base, ee)
+    x=p(1);
+    y=p(2);
+    z=p(3);
+    y1 = -base;
+    y=y-ee;
+    a = (x^2 + y^2 + z^2 + servo_arm*servo_arm - end_effector_rod*end_effector_rod - y1*y1)/(2*z);
     b = (y1-y)/z;
-    d = -(a + b*y1)*(a + b*y1) + upper*(b*b*upper + upper);
-    yj = (y1 - a*b - math.sqrt(d))/(b*b + 1);
+    d = -(a + b*y1)*(a + b*y1) + servo_arm*(b^2*servo_arm + servo_arm);
+    yj = (y1 - a*b -sqrt(d))/(b^2 + 1);
     zj = a + b*yj;
-    theta = 180.0*math.atan(-zj/(y1-yj))/math.pi;
+    theta = atan(-zj/(y1-yj));
     if yj > y1
-      theta = theta + 180;
+      theta = theta + pi;
     end
 end
--- MATLAB/deltaIK.m
+++ MATLAB/deltaIK.m
@@ -1,14 +1,14 @@
-function omega = deltaIK(p, upper, lower)
+function omega = deltaIK(p)
     % DELTAIK(p, upper, lower)
     % This function takes in a vector of the x, y, z position of the
     % desired position (p) and the upper and lower leg lengths. It returns
     % omega, a vector of the joint angles
 
-    % upper=50;
-    % lower=100;
-    Rf = 500/2;
-    Rm = 300/2;
-
+    servo=100;
+    ee_bar=150;
+    ee = 48;
+    base = 75;
+    
     x = p(1);
     y = p(2);
     z = p(3);
@@ -19,9 +19,16 @@ function omega = deltaIK(p, upper, lower)
     p3 = [x*cosd(120) - y*sind(120)
           y*cosd(120) + x*sind(120)
           z];
-
-    theta1 = calc_yz(p, upper, lower, Rf, Rm);
-    theta2 = calc_yz(p2, upper, lower, Rf, Rm);
-    theta3 = calc_yz(p3, upper, lower, Rf, Rm);
-    omega = [theta1 theta2 theta3]';
+    plot3([x,p2(1),p3(1)],[y,p2(2),p3(2)],[z,z,z])
+%     cos120 =cosd(120);
+%     sin120 = sind(120);
+    theta1 = rad2deg(calcYZ(p,ee_bar, servo , base, ee))+30;
+    theta2 = rad2deg(calcYZ(p2, ee_bar, servo, base, ee))+30;
+    theta3 = rad2deg(calcYZ(p3, ee_bar, servo, base, ee))+30;
+    
+%     theta1 = calcYZ([x0, y0, z0],upper, lower, Rf, Rm);
+%     theta2 = calcYZ([x0*cos120 + y0*sin120, y0*cos120 - x0*sin120, z0],upper, lower, Rf, Rm); % rotate +120 deg
+%     theta3 = calcYZ([x0*cos120 - y0*sin120, y0*cos120 + x0*sin120, z0],upper, lower, Rf, Rm); % rotate -120 deg
+    omega = [theta1 theta2 theta3]'*-1;
+    deg2rad([omega(1);omega(3);omega(2)])
 end
